b4da41d6937d47166fb17342c719b5e2a378e2d1
otvertka <otvertka123@ukr.net>
2023-08-10 12:14:28 +0300

    defer

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,17 +1,38 @@
-import { json, redirect, useRouteLoaderData } from "react-router-dom";
+import {
+  json,
+  redirect,
+  useRouteLoaderData,
+  defer,
+  Await,
+} from "react-router-dom";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventDetailPage() {
-  const data = useRouteLoaderData("event-detail");
+  const { event, events } = useRouteLoaderData("event-detail");
 
-  return <EventItem event={data.event} />;
+  return (
+    <>
+      {/* 2 блока ожидания */}
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading////</p>}>
+        <Await resolve={event}>
+          {(loadEvent) => <EventItem event={loadEvent} />}
+        </Await>
+      </Suspense>
+
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading-----</p>}>
+        <Await resolve={events}>
+          {(loadEvents) => <EventsList events={loadEvents} />}
+        </Await>
+      </Suspense>
+    </>
+  );
 }
 
 export default EventDetailPage;
 
-export async function loader({ request, params }) {
-  const id = params.eventId;
-
+async function loadEvent(id) {
   const response = await fetch("http://localhost:8080/events/" + id);
 
   if (!response.ok) {
@@ -22,8 +43,32 @@ export async function loader({ request, params }) {
       }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
   }
+}
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: "Could not fetch events." };
+    // throw new Response(JSON.stringify({ message: "Could not fetch evens" }), {
+    //   status: 500,
+    // });
+    return json({ message: "Could not fetch evens" }, { status: 500 });
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  return defer({
+    event: await loadEvent(id),
+    events: loadEvents(),
+  });
 }
 
 export async function action({ params, request }) {
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,19 +1,10 @@
-import { Link, useNavigate } from "react-router-dom";
+import PageContent from "../components/PageContent";
 
 function HomePage() {
-  const navigate = useNavigate();
-
-  function navigateHandler() {
-    navigate("/events");
-  }
   return (
-    <>
-      <h1>My Home Page</h1>
-      <p>
-        Go to <Link to="events"> list of events</Link>
-      </p>
-      <button onClick={navigateHandler}>Navigate</button>
-    </>
+    <PageContent title="Welcome!">
+      <p>Browse all our amazing events!</p>
+    </PageContent>
   );
 }
 
